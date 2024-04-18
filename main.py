3a1d36ffb0a6bea11aa66290f980b52ce34471a1
Nikhil-Jaiz <98412087+Nikhil-Jaiz@users.noreply.github.com>
2024-04-18 18:07:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pandas as pd
+import csv
+import pymysql
 dF=pd.read_csv("Sample.csv")
 print(dF)
+
+
+with open("Sample.csv",'r')  as file:
+  reader=csv.reader('Sample.csv')
+  connection = pymysql.connect(host='localhost', user='root',  password='Year' ,db='nikhildb')
+  cursor=connection.cursor()
+  for row in reader:
+           cursor.execute('INSERT INTO csv (`Region`, `Country`, `Item Type`, `Sales Channel`, `Order Priority`, `Order Date`, `Order ID`, `Ship Date`, `Units Sold`, `Unit Price`, `Unit Cost`, `Total Revenue`, `Total Cost`, `Total Profit`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',row)
+           connection.commit()
+
+
+cursor.close()
+connection.close()
